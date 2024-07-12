bb75ebadcaa0c8d5776cce557dac7bacbfc8d4a4
nhannht <nhanclassroom@gmail.com>
2024-07-12 13:55:31 +0700

    feat: add script to generate sample vault

---
+++ script/gen-sample-md.mjs
@@ -0,0 +1,63 @@
+#!/usr/bin/env zx
+import 'zx/globals'
+import {mkdirSync, writeFileSync} from "fs";
+import {existsSync, rm, rmdirSync} from "node:fs";
+import {faker} from '@faker-js/faker'
+
+const vaultName = argv.vault ? argv.vault : (() => {
+	console.log("need --vault name argument")
+	process.exit(0)
+})()
+
+
+if (!existsSync((vaultName))) {
+	mkdirSync(vaultName)
+} else {
+	rmdirSync(vaultName, {recursive: true})
+	mkdirSync(vaultName)
+}
+
+const numberOfFiles = argv.fileN ? argv.fileN : 1
+const numberOfEl = argv.taskN ? argv.taskN : 10
+
+
+
+const filePaths = faker.helpers.uniqueArray(()=>{
+	return path.join(vaultName,faker.system.filePath())
+},numberOfFiles)
+
+
+
+function generateOneTaskOrParagraphs(){
+	return faker.helpers.weightedArrayElement([
+		{
+			weight: 3,
+			value: `- [ ] ${faker.lorem.slug()}`
+		},
+		{
+			weight: 3,
+			value: `${faker.lorem.paragraphs({min: 1, max: 5}, "\n\n")}`
+		},
+		{
+			weight: 2,
+			value: `- [ ] ${faker.lorem.slug()} from ${faker.date.past()} to ${faker.date.future()}`
+		},
+		{
+			weight: 2,
+			value: `- [ ] ${faker.lorem.slug()} at ${faker.date.anytime()}`
+		}
+	])
+
+}
+
+filePaths.forEach((f, i) => {
+	let element = []
+	for (let i = 0; i< numberOfEl;i++){
+		element.push(generateOneTaskOrParagraphs())
+	}
+	const fileDir = path.dirname(f)
+	if (!existsSync(fileDir)) mkdirSync(fileDir,{recursive:true})
+	writeFileSync(`${f}`,element.join("\n"))
+
+})
+
